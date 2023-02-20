81f8da5c8f301fb3b99b577878552e4e80bf9a3d
Pymode-Dev <pymode000@gmail.com>
2023-02-20 15:03:49 -0800

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints 0123456789
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		printf("%d", number);
+	}
+	printf("\n");
+}
