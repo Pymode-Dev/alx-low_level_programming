8da695a6d37a682c8f0c8958180a4b68db414244
Pymode-Dev <pymode000@gmail.com>
2023-02-20 14:40:49 -0800

    Print alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,13 +11,13 @@ int main(void)
 	char upper;
 	char lower;
 
-	for (upper = 'A'; upper <= 'Z'; upper++)
-	{
-		putchar(upper);
-	}
 	for (lower = 'a'; lower <= 'z'; lower++)
 	{
 		putchar(lower);
+	}
+	for (upper = 'A'; upper <= 'Z'; upper++)
+	{
+		putchar(upper);
 	}
 
 	putchar('\n');
