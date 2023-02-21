1037a350ecb06d294252929a47733450261cc6de
Pymode-Dev <pymode000@gmail.com>
2023-02-21 15:15:21 -0800

    putchar alphabet in function

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,14 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	char j;
 
 	for (i = 0; i < 10; i++)
 	{
-		_putchar(print_alphabet());
+		for (j = 'a'; j <= 'z'; j++) 
+		{
+			_putchar(j);
+		}
 	}
 	_putchar('\n');
 
