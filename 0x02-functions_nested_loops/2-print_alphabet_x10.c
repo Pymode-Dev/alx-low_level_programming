48c7d0693662e176e741346428421a75c347cedc
Pymode-Dev <pymode000@gmail.com>
2023-02-21 15:09:44 -0800

    putchar alphabet in function

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times.
+ *
+ * Return: Always 0.
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(print_alphabet());
+	}
+	_putchar('\n');
+
+	return (0);
+}
