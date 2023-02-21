9d9c919ac10479d62f817c09dcb737395671de94
Pymode-Dev <pymode000@gmail.com>
2023-02-21 14:47:31 -0800

    putchar alphabet in alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Alwaays 0.
+ */
+
+int main(void)
+{
+	void print_alphabet()
+	{
+		char alpha;
+
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			putchar(alpha);
+		}
+		putchar('\n');
+	}
+	print_alphabet();
+
+	return (0);
+}
