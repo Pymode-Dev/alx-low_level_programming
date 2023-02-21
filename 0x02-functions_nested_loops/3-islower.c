6459d2ee4448c8eb35be808ce2a1e266ca0a61b3
Pymode-Dev <pymode000@gmail.com>
2023-02-21 15:56:51 -0800

    putchar alphabet in function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - print 1 or 0 if it is lowercase alphabet
+ *
+ * Return: Always 0.
+ */
+
+int _islower(char c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
