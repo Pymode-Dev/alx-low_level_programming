23d639b767dd9a41d021a2d36d2456e17001c6cb
Pymode-Dev <pymode000@gmail.com>
2023-02-20 16:24:51 -0800

    Print comb of 2 numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Print Combination of 2 numbers.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (m = n + 1; m < 10; m++)
+		{
+			putchar(n + '0');
+			putchar(m + '0');
+			if (!((n == 8) && (m == 9)))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
