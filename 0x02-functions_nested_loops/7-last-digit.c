ee7fe3094600feede312e5101a87f14d468f16d8
Pymode-Dev <pymode000@gmail.com>
2023-02-21 16:50:29 -0800

    print last digit

---
+++ 0x02-functions_nested_loops/7-last-digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: int
+ * Return: Always 0.
+ *
+ */
+
+int print_last_digit(int n)
+{
+	int x;
+
+	x = n % 10;
+	return (x);
+}
