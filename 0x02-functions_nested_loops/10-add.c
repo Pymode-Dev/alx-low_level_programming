6f145d3d9d40b4cfef92961c807ed0faeb2119e8
Pymode-Dev <pymode000@gmail.com>
2023-02-21 17:40:18 -0800

    Addition function.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - add two numbers together
+ * @num1: int
+ * @num2: int
+ *
+ * Return: Always 0.
+ *
+ */
+
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+
+	return (result);
+}
