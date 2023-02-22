de9e3b1b07f079094d00dbf7f4aa214900e4a7f6
Pymode-Dev <pymode000@gmail.com>
2023-02-21 18:16:30 -0800

    updated

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,11 +8,35 @@
  * Return: Always 0.
  */
 
-void print_to_age(int n)
+void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i == 98)
+			{
+				printf("%w\n", i);
+			} else
+			{
+				printf("%d, ", 1);
+			}
+		}
+	} else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i == 98)
+			{
+				printf("%d\n", i);
+			} else
+			{
+				printf("%d, ", i);
+			}
+		}
+	} else
 	{
 		printf("%d, ", i);
 	}
