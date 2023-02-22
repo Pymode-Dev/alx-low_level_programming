0dc3e4bec60425a484ae256437ef0037a09e8b94
Pymode-Dev <pymode000@gmail.com>
2023-02-22 06:47:21 -0800

    Debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,9 @@
 
 #include <stdio.h>
 
-void postive_or_negative(int i);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int year);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif /* MAIN_H */
