8f3fcdc68489a84a8586b7283e804767826c4485
Pymode-Dev <pymode000@gmail.com>
2023-02-21 16:29:59 -0800

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - print sign base on the rule
+ * @n: int
+ * Return: Always 0.
+ *
+ */
+
+int print_sign(int n)
+{
+	int x;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		x = 1;
+	} else if (n == 0)
+	{
+		_putchar('0');
+		x = 0;
+	} else
+	{
+		_putchar('-');
+		x = -1;
+	}
+	return (x);
