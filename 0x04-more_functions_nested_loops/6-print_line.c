b2ce44706711eb15200e522a8b95febe8750ac90
Pymode-Dev <pymode000@gmail.com>
2023-02-27 02:43:05 -0800

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - prints line
+ *@n: int
+ *Return: 0.
+ */
+
+void print_line(int n)
+{
+	if (n < 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		for (int i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+}
