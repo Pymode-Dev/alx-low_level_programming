cdbfe486ab0e46a0e30e6cb9de0f5b6ac450e4d6
Pymode-Dev <pymode000@gmail.com>
2023-02-27 01:36:04 -0800

    uppercase

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - print number from 0 - 9
+ *
+ * Return: 0.
+ */
+
+void print_numbers()
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+}
