2787c9ac6223791918512a9a17d693d3ac5ec4f1
Pymode-Dev <pymode000@gmail.com>
2023-02-28 15:02:59 -0800

    Return length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *  _strlen - return the length of a string.
+ *  @*s: char
+ *  Return: 0.
+ */
+
+int _strlen(char *s)
+{
+	int length;
+	length = sizeof(s);
+
+	return (length);
+}
