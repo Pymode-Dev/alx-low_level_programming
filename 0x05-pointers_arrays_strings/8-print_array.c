8ba2487069bbb3830ca8199ed37b60bcfa199773
Pymode-Dev <pymode000@gmail.com>
2023-02-28 16:47:22 -0800

    Print n element in an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - print n element
+ * @a: the array
+ * @n: the size of the array
+ * Return: 0.
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		if (i < (n - 1));
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	printf("\n");
+}
