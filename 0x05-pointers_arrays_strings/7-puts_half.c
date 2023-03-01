8b33c0b27c840471b00f37d3a973e6aba8f03ff7
Pymode-Dev <pymode000@gmail.com>
2023-02-28 16:34:21 -0800

    Puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - print half of the string
+ * @str: the string to print
+ * Return: 0.
+ */
+
+void puts_half(char *str)
+{
+	int i = 0, j = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	
+	j = i / 2;
+
+	while (j != '\0')
+	{
+		_putchar(str[j]);
+		j++;
+	}
+}
