8218a94495349cc600832618f88e7eb7fb647b15
Pymode-Dev <pymode000@gmail.com>
2023-02-28 16:23:39 -0800

    Puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other string
+ * @str: string to print
+ * Return: 0.
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[j] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
