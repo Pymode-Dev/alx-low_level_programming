f1881f2f0f7eae5309bcd15abb237c76169567ad
Pymode-Dev <pymode000@gmail.com>
2023-03-03 05:34:19 -0800

    Just one line

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -17,10 +17,6 @@ int main(void)
 	 * - you are not allowed to code anythinh else than this line of code
 	 */
 	*(p + 5) = 98;
-	printf("The address of p is %p\n", &p);
-	printf("The address of a is %p\n", &a);
-	printf("The address of n is %p\n", &n);
-	printf("The value of a=%p, n = %d, p = %d\n", a, n, *p);
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
