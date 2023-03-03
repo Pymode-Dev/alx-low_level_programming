25fdd61ab009898fab2f9bf212df300ad8692ad8
Pymode-Dev <pymode000@gmail.com>
2023-03-02 21:20:47 -0800

    Capitalizing String

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,7 @@
 char *cap_string(char *s)
 {
 	int i, j;
-	char separator[] = "/t/n,;.!?\"(){}";
+	char separator[] = " /t/n,;.!?\"(){}";
 
 	i = 0;
 
@@ -18,16 +18,16 @@ char *cap_string(char *s)
 		if (s[0] >= 'a' && s[0] <= 'z')
 			s[0] -= 32;
 
-			j = 0;
+		j = 0;
 
-			while (separator[j] != '\0')
+		while (separator[j] != '\0')
+		{
+			if (s[i] == separator[j] && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
 			{
-				if (s[i] == separator[j] && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
-				{
-					s[i + 1] -= 32;
-				}
-				j++;
+				s[i + 1] -= 32;
 			}
+			j++;
+		}
 			i++;
 	}
 
