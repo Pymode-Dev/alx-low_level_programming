78f43bec912e03221699c6d92de2433c6ccba572
Pymode-Dev <pymode000@gmail.com>
2023-03-02 18:52:29 -0800

    String to uppercase

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes a string
+ * @s: string
+ * Return: capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char separator[] = "/t/n,;.!?\"(){}";
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[0] >= 'a' && s[0] <= 'z')
+			s[0] -= 32;
+
+			j = 0;
+
+			while (separator[j] != '\0')
+			{
+				if (s[i] == separator[j] && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+				{
+					s[i + 1] -= 32;
+				}
+				j++;
+			}
+			i++;
+	}
+
+	return (s);
+}
