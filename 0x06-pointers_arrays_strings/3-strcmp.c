a05da987a858d5ed857944a7943d9b63a3a31b0a
Pymode-Dev <pymode000@gmail.com>
2023-03-02 17:52:03 -0800

    compare string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: -15, 0, or 15
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int x = 0;
+
+	while ((*s1 != '\0' && *s2 !='\0') && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 != *s2)
+	{
+		x = *s1 - *s2;
+	}
+
+	return (x);
+}
