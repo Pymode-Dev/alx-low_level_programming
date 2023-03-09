f372544954e52c631eab8af64fbdbc63702b2a0b
Pymode-Dev <pymode000@gmail.com>
2023-03-08 23:39:28 -0800

    String span

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr: locates a sub-string
+ * @haystack: Haystack
+ * @needle: Needle
+ * Return: pointer to location of substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		i = 0;
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			} while (haystack[i] == needle[i]);
+		}
+		haystack++;
+	}
+	return (0);
+}
