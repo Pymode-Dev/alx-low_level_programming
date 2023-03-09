a2e56b960217ac9d77bcb3573aada454182b3fb4
Pymode-Dev <pymode000@gmail.com>
2023-03-08 23:59:51 -0800

    print sum of square matrix

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of the two diagonals of a square matrix
+ * @a: square matrix
+ * @size: size of diagonals
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	i = 0;
+
+	while (i < size)
+	{
+		sum1 += *(a + (size * i) + i);
+		sum2 += *(a + (size * i + size - 1 - i));
+		i++;
+	}
+		printf("%d, %d\n", sum1, sum2);
+}
