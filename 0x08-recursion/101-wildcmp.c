7af2e08b81b768abf49a682a244659ebf8ccb75a
Pymode-Dev <pymode000@gmail.com>
2023-03-09 06:52:18 -0800

    Wild comparism

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,18 @@
+# include "main.h"
+
+/**
+ * wildcmp -compares strings
+ * @s1: pointer to string
+ * @s2: pointer to string
+ * Return: 0 or 1
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '\0')
+		return (*s1 == '\0');
+	if (*s2 == *s1)
+		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+	return (0);
+}
