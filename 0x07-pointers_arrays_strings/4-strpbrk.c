7cf447b88e2abbfcb46f19ea5b46463088968869
Pymode-Dev <pymode000@gmail.com>
2023-03-08 23:30:14 -0800

    String span

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string
+ * @accept: comaparison string
+ * Return: pointer to byte where a match is found
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (accept[i] == *s)
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return ('\0');
+}
