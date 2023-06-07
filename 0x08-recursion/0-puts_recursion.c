b6176a8ee588562e48cf6c67bbf45cceeeb57ffa
Pymode-Dev <pymode000@gmail.com>
2023-06-07 06:51:09 -0700

    Add docstring to the file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * _puts_recursion - put character on to the screen recursively
+ * s: the string to print recursively
+ * Return: void
+ */
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
