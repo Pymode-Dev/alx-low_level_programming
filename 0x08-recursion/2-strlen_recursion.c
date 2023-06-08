791be558b7e2ca593ff81e8d21332574fc3df60c
Pymode-Dev <pymode000@gmail.com>
2023-06-07 19:24:14 -0700

    The length of a string recursively

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - return the length of a string
+ * @str: string to calculates its length
+ * Return: int
+ */
+int i = 0;
+
+int _strlen_recursion(char *str)
+{
+	if (*s != '\0')
+	{
+		i++;
+		_strlen_recursiom(str + 1);
+	}
+	return (i);
+}
