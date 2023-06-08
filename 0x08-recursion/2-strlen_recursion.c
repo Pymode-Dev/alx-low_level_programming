e18e088ff1d80dc3828e28517a884a0f0430314e
Pymode-Dev <pymode000@gmail.com>
2023-06-07 22:37:14 -0700

    Correct a typo

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,14 +5,12 @@
  * @str: string to calculates its length
  * Return: int
  */
-int i = 0;
 
 int _strlen_recursion(char *str)
 {
 	if (*str != '\0')
 	{
-		i++;
-		_strlen_recursion(str + 1);
+		return 1 + _strlen_recursion(str + 1);
 	}
 	return (i);
 }
