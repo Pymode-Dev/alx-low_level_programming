28b554b8c56acda9ecea49ab894795898822451e
Pymode-Dev <pymode000@gmail.com>
2023-06-08 02:57:55 -0700

    Is it a palindrome?

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * _strlen_recursion - get the length of a string
+ * @s: the string
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * _pal - check if the string is palindrome
+ * @str: the string
+ * @len: the length of the string
+ * Return: int
+ */
+
+int _pal(char *str, int len)
+{
+	if (len < 1)
+		return (1);
+	if (*str == *(str + len))
+		return (_pal(str + 1, len - 2));
+	return (0);
+}
+
+/**
+ * is_palindrome - the main function that check
+ * @str: the string to check
+ * Return: int
+ */
+
+int is_palindrome(char *str)
+{
+	int l = _strlen_recursion(str);
+
+	return (_pal(str, l - 1));
+}
