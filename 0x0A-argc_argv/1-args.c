09168f5b5193500493a7fabfb3299b826490f03e
Pymode-Dev <pymode000@gmail.com>
2023-06-09 16:45:39 -0700

    Number of argument is?wq

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program to print the number of arguments.
+ * @argc: the number of argument
+ * @argv: the array of arguments.
+ * Return: 0
+ */
+
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
