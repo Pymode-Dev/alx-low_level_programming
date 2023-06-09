20c51303e37bf220d606a9032ad0bfe32abf8359
Pymode-Dev <pymode000@gmail.com>
2023-06-09 16:35:08 -0700

    My name is?

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - it contains the command line argument
+ * @argc: the number of args
+ * @argv: the array of args
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
