d5cbf095f14037d91b8ca97432cf100b117e7fb9
Pymode-Dev <pymode000@gmail.com>
2023-06-09 16:50:33 -0700

    Push a file that will print all arguments passed into it

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all argument passed into it in newline
+ * @argc: the number of args
+ * @argv: the array of args
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (i; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
