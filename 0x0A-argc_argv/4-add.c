a1fa06fd610970fd03819d383e8202b75b58a0be
Pymode-Dev <pymode000@gmail.com>
2023-06-09 17:28:59 -0700

    Push a file that will print addition of numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - addition of two numbers
+ * @argc: number of args
+ * @argv: array of args
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int x = 0;
+	int a = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+
+	for (int i = 1; i < argc; i++)
+	{
+		for (int j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		a += atoi(argv[i]);
+	}
+	printf("%d\n", a);
+
+	return (x);
+}
