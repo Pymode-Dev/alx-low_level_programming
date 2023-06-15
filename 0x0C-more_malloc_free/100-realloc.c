aedfa6445d8029a318adeb16678e2bd597ddf7cf
Pymode-Dev <pymode000@gmail.com>
2023-06-15 14:32:54 -0700

    _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -23,6 +23,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		if (new == NULL)
 			return (NULL);
 	}
+
+	if ((new_size == 0) && (ptr != NULL))
+	{
+		free(ptr);
+		return (NULL);
+	}
+
 	if (new_size == 0 && ptr == NULL)
 	{
 		free(ptr);
