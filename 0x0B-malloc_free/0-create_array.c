8a10065fde86ed634af1d650a6c251b927baff85
Pymode-Dev <pymode000@gmail.com>
2023-06-15 04:48:44 -0700

    Allocate string size

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	char *array = (char *) malloc(size * sizeof(char));
 	unsigned int i = 0;
 
-	if (array == 0)
+	if (array == 0 || size == 0)
 		return (NULL);
 
 	while (i < size)
