61ed1c0c4cab912c1dcb5444a3f1a9ca422d0d80
Pymode-Dev <pymode000@gmail.com>
2023-06-15 09:00:42 -0700

    It is not bragging if you can back it up

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 
 
+/**
+ * free_grid - Free the grid from memory block
+ * @grid: the grid to free
+ * @height: The height of the grid
+ * Return: void
+ */
+
 void free_grid(int **grid, int height)
 {
 	int i = 0;
