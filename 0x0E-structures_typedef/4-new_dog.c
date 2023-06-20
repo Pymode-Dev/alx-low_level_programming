bb3ede0ea0b89ca7bad6d5654affa4bf089c825f
Pymode-Dev <pymode000@gmail.com>
2023-06-20 14:03:47 -0700

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,46 @@
 #include "dog.h"
 
 
+int _str_len(char *str);
+char *_strcpy(char *dest, char *src);
+
+/**
+ * _strlen - calculates the length of a string
+ * @str: The string
+ * Return: int
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strcpy - copy one string to another
+ * @dest: the argument to copy the string to
+ * @src: the source
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
 /**
  * new_dog - creates a new dog
  * @name: name of the new dog
@@ -16,14 +56,32 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new;
 
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(dog_t));
 
 	if (new == NULL)
 		return (NULL);
 
-	new->name = name;
+
+	new->name = malloc(_strlen(name) * sizeof(char));
+	if (new->name == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->owner = malloc(_strlen(owner) * sizeof(char));
+	if (new->owner == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->name = _strcpy(new->name, name);
 	new->age = age;
-	new->owner = owner;
+	new->owner = _strcpy(new->owner, owner);
 
 	return (new);
 }
