160e748c15d4db9b196d59b5b8adddde3c40831d
Pymode-Dev <pymode000@gmail.com>
2023-06-20 05:42:07 -0700

    A dog is the only thing on earth that loves you more that you love yourself

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: the struct pointer
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ * Return: NULL or dog
+ */
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+}
