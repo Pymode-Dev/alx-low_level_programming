95ee2d5b25937131a7f1f34a18bcd9efe648b3f7
Pymode-Dev <pymode000@gmail.com>
2023-06-20 06:14:23 -0700

    A dog will teach you unconventional love. If you can have that in your life, things won't be too bad

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - print the dog struct
+ * @d: the struct dog
+ * Return: void
+ */
+
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("nil\n");
+
+		if (d->age != 0)
+			printf("Age: %.1f\n", d->age);
+		else
+			printf("nil\n");
+
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("nil\n");
+	}
+}
