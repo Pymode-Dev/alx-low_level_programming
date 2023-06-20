c6b869dcccdb4bf647468746dc302921ecc9405e
Pymode-Dev <pymode000@gmail.com>
2023-06-20 05:18:57 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - Information about a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the owner
+ *
+ * Desc: stores a dog info
+ */
+
 struct dog
 {
 	char *name;
