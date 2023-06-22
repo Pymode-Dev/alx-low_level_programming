cdbb28b34355bdef2b54c542e68bd43dfa8d386c
Pymode-Dev <pymode000@gmail.com>
2023-06-21 17:38:45 -0700

    Function pointer in string

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - print name with any case
+ * @name: the name of the person
+ * @f: the function pointer
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef _HEADERS_
+#define _HEADERS_
+
+#include <stdio.h>
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
