fa570418de83569e6758e331c48b043e0a6099f1
Pymode-Dev <pymode000@gmail.com>
2023-06-23 03:42:06 -0700

    To be is to be the value of a variable

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print the number with separator
+ * @separator: The separator to print within the numbers
+ * @n: the number of integer to print
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i = n, j;
+	int number;
+	va_list list;
+
+	va_start(list, n);
+
+	if (separator == NULL)
+		exit(100);
+
+	j = 0;
+
+	for (; j <= i; j++)
+	{
+		number = va_arg(list, int);
+		printf("%d", number);
+		if (separator != NULL && j < (i - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(list);
+}
