cc253b6ee1e549886871a9534e84cc425adb0dc1
Pymode-Dev <pymode000@gmail.com>
2023-07-03 15:40:58 -0700

    0-Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: the linked list
+ * Return: size_t
+ */
+
+
+size_t print_list(const list_t *h)
+{
+       long unsigned node;
+       const list_t *ptr = NULL;
+       ptr = h;
+
+	node = 0;
+
+	while (ptr != NULL)
+	{
+
+		if (ptr->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+			printf("[%d] %s\n", ptr->len, ptr->str);
+
+		ptr = ptr->next;
+		node++;
+	}
+
+	return (node);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef __LINKED__
+#define __LINKED__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif /* __LINKED__ */
