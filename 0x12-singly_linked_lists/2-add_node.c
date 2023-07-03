eb0f9a304b8175fc9f39852c344f8e8fc327af90
Pymode-Dev <pymode000@gmail.com>
2023-07-03 16:27:56 -0700

    2-add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+int str_len(const char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+
+/**
+ * add_node - It adds a new node at the beginning of a linked list
+ * @head: The pointer address
+ * @str: The data to store
+ *
+ * Return: address of new element or NULL
+ */
+
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *ptr;
+
+	ptr = malloc(sizeof(list_t));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	ptr->str = strdup(str);
+	ptr->len = str_len(str);
+	ptr->next = (*head);
+	(*head) = ptr;
+
+	return (*head);
+}
