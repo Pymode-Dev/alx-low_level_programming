b79b67b4192f886ce0d62860c5e4c8570dba6892
Pymode-Dev <pymode000@gmail.com>
2023-07-05 16:13:07 -0700

    4-Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - Function that frees a linked list
+ * @head: The pointer to the first node
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	while (head->next != NULL)
+	{
+		ptr = head->next;
+		free(head);
+		head = ptr;
+	}
+	free(ptr);
+}
--- 0x13-more_singly_linked_lists/main.c
+++ 0x13-more_singly_linked_lists/main.c
@@ -15,6 +15,8 @@ int main()
 	add_nodeint_end(&head, 402);
 	add_nodeint_end(&head, 1024);
 	print_listint(head);
+	free_listint(head);
+	head = NULL;
 
 	return(0);
 }
