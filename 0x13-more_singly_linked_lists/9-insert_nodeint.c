20d028b3fd28ef77d40466df90d45b8e43790867
Pymode-Dev <pymode000@gmail.com>
2023-07-09 16:04:39 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,6 +35,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (nptr);
 	}
 
+	if (idx != 0)
+	{
+		ptr = (*head);
+		for (; i < idx - 1 && ptr != NULL; i++)
+			ptr = ptr->next;
+		if (ptr == NULL)
+			return (NULL);
+	}
+
 	while (ptr)
 	{
 		i++;
