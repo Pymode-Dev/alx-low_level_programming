3627ce01e8a769eb7626cef93886f9840066d437
Pymode-Dev <pymode000@gmail.com>
2023-07-09 19:08:00 -0700

    Delete

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,9 +1,41 @@
-#include <stdio.h>
+#include "lists.h"
 
 
 /**
  * delete_nodeint_at_index - Deletes a node fom a specific position
  * @head: pointer to pointer of first node
  * @index: the position
- *
+ *Return: int
  */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *ptr, *nptr;
+	unsigned int i;
+
+	if ((*head) == NULL || head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		ptr = (*head)->next;
+		free(*head);
+		(*head) = ptr;
+		return (1);
+	}
+
+	ptr = (*head);
+
+	for (i = 0; i < (index - 1); i++)
+	{
+		if (ptr->next == NULL)
+			return (-1);
+		ptr = ptr->next;
+	}
+
+	nptr = ptr->next;
+	ptr->next = nptr->next;
+	free(nptr);
+
+	return (1);
+}
