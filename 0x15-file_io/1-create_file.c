bf9c67fa0afec9f99a32d9480cafdabf50a0268f
Pymode-Dev <pymode000@gmail.com>
2023-07-10 17:35:03 -0700

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+/**
+ * create_file - function that creates file
+ * @filename: pointer to the function
+ * @text_content: the text to write
+ * Return: 1 if succeed -1 if fails.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, file;
+
+	if (filename == NULL)
+		return (-1);
+	
+	if (text_content == NULL && filename != NULL)
+		fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	file = write(fd, text_content, sizeof(text_content));
+
+	if (fd == -1 || file == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
