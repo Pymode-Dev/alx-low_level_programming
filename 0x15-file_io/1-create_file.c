12c9d9ebd30f566857eb29a909f6efec4884d2ce
Pymode-Dev <pymode000@gmail.com>
2023-07-10 17:48:49 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,24 @@
 #include "main.h"
 
+int _strlen(char *str);
+
+/**
+ * _strlen - length
+ * @str: the string
+ * Return: int
+ */
+
+int _strlen(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	return (i);
+}
 
 /**
  * create_file - function that creates file
@@ -10,13 +29,15 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, file;
+	int fd, file, len;
+
+	len = _strlen(text_content);
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	file = write(fd, text_content, sizeof(text_content));
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	file = write(fd, text_content, len);
 
 	if (fd == -1 || file == -1)
 		return (-1);
