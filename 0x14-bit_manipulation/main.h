900736cdda4c6e3ff94d2fad2e1d2aaa1d7bbf03
Oyewale Joseph <pymode000@gmail.com>
2023-08-03 19:49:51 -0700

    0.0

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef __BIT__
+#define __BIT__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int _putchar(char c);
+unsigned int binary_to_unit(const char *b);
+
+#endif /* __NIT__ */
