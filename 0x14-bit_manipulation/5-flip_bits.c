8c1802aae2fa433f7b9413316c8bb2a57049ca79
Oyewale Joseph <pymode000@gmail.com>
2023-08-04 18:57:37 -0700

    101 - how many bits to flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * flip_bits - function that returns the number of bits you would need to flip
+ * @n: the value
+ * @m: the index
+ * Return: int
+ */
+
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int dbits = n ^ m;
+	unsigned int counter = 0;
+
+
+	while (dbits)
+	{
+		if (dbits & 1)
+			counter++;
+		dbits >>= 1;
+	}
+	
+	return (counter);
+}
--- 0x14-bit_manipulation/main.c
+++ 0x14-bit_manipulation/main.c
@@ -4,16 +4,16 @@ int main(void)
 {
 	unsigned long int n;
 
-	n = 1024;
-	clear_bit(&n, 10);
+	n = flip_bits(1024, 1);
 	printf("%lu\n", n);
 
-	n = 0;
-	clear_bit(&n, 10);
+	n = flip_bits(402, 98);
 	printf("%lu\n", n);
 
-	n = 98;
-	clear_bit(&n, 1);
+	n = flip_bits(1024, 3);
+	printf("%lu\n", n);
+
+	n = flip_bits(1024, 1025);
 	printf("%lu\n", n);
 
 	return (0);
