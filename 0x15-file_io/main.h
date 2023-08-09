c2dc666b11d8252f988d09b253e765076eb62c67
Oyewale Joseph <pymode000@gmail.com>
2023-08-08 18:51:52 -0700

    Tread lightky, she is near

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,8 +4,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 
 
-#endif /* __MAIN++ */
+#endif /* __MAIN__ */
