9ac1385764e8bdecea8722aebbb468e9393ac2be
Pymode-Dev <pymode000@gmail.com>
2023-09-18 04:02:32 -0700

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/main.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int main()
+{
+	printf("%d\n", _strlen("my Dyn Lib"));
+	return (EXIT_SUCCESS);
+}
---
+++ 0x18-dynamic_libraries/strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+int _strlen(char *s)
+{
+	int n = 0;
+
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+
+	return (n);
+}
