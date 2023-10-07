010acc5da021971fc2e53d1d421e85541dbbd0fc
Pymode-Dev <pymode000@gmail.com>
2023-10-06 21:32:06 -0700

    2. Key -> index

--- 0x1A-hash_tables/main.c
+++ 0x1A-hash_tables/main.c
@@ -2,13 +2,18 @@
 
 int main(void)
 {
+	unsigned long int hash_t;
 	char *s;
 
+	hash_t = 1024;
 	s = "cisfun";
 	printf("%lu\n", hash_djb2((unsigned char *)s));
+	printf("%lu\n", key_index((unsigned char *)s, hash_t));
 	s = "Don't forget to tweet today";
 	printf("%lu\n", hash_djb2((unsigned char *)s));
+	printf("%lu\n", key_index((unsigned char *)s, hash_t));
 	s = "98";
 	printf("%lu\n", hash_djb2((unsigned char *)s));
+	printf("%lu\n", key_index((unsigned char *)s, hash_t));
 	return (EXIT_SUCCESS);
 }
